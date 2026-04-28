406863a85be5970e6bcbb529c00ec64960219cb0
meruto187 <meruto187@github.com>
2026-04-28 17:15:37 +0300

    chore: remove electron node_modules from git, update .gitignore

--- electron-app/out/preload/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-  showNotification: (title, body) => electron.ipcRenderer.send("show-notification", { title, body }),
-  showWindow: () => electron.ipcRenderer.send("show-window")
-});
